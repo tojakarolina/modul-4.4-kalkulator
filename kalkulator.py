b556292e3c8dee2fc2c192ddae70af795f395fba
tojakarolina <karolina.przytula1997@gmail.com>
2023-01-10 16:52:45 +0100

    Add input and logging info kalkulator.py

--- kalkulator.py
+++ kalkulator.py
@@ -1,20 +1,33 @@
 import sys
+import logging
+logging.basicConfig(level=logging.INFO)
 
-def shopping(function, num_1, num_2):
+def calculate_2_numbers(function, num_1, num_2):
     if function == 1:
-            addiction = num_1 + num_2
-            print(addiction)
+            calculation = num_1 + num_2
     elif function == 2:
-            subtraction = num_1 - num_2
-            print(subtraction)
+            calculation = num_1 - num_2
     elif function == 3:
-            multiplication = num_1*num_2
-            print(multiplication)
+            calculation = num_1*num_2
     elif function == 4:
-            division = num_1 / num_2
-            print(division)
-    return function
+            calculation = num_1 / num_2
+    else: 
+            exit(1)
+    return calculation
 
 if __name__ == "__main__":
-    print(sys.argv[1:])
-    shopping(1, 2, 3)
+    pytanie = int(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:"))
+    skl_1 = float(input("Podaj składnik 1. "))
+    skl_2 = float(input("Podaj składnik 2. "))
+    odp = ""
+    if pytanie == 1:
+            odp = "Dodaję"
+    elif pytanie == 2:
+            odp = "Odejmuję"
+    elif pytanie == 3:
+            odp = "Mnożę"
+    elif pytanie == 4:
+            odp = "Dzielę"
+    logging.info(f"{odp} {skl_1:.2f} i {skl_2:.2f}")
+    print(f"Wynik to {calculate_2_numbers(pytanie,skl_1,skl_2)}")
+
